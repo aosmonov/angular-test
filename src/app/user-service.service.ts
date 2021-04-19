85bb685393175e923c16e955534dbee96dd3d785
Aibek Osmonov <aibek.osmonov@demirbank.kg>
2021-04-19 18:11:50 +0600

    Initial commit

---
+++ src/app/user-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor() { }
+}
